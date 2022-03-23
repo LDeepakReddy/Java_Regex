2f17a3a3d7df6aafe0760c9cf71d78c442c56d4b
LDeepakReddy <l.deepakreddy.com>
2022-03-24 03:31:30 +0530

    Removed Extra code which is not useful

--- src/com/blz/java/regularexpression/Email.java
+++ src/com/blz/java/regularexpression/Email.java
@@ -8,7 +8,7 @@ public class Email {
     public static void main(String[] args) {
         System.out.println("Welcome to the Email Validation program");
         String email = "abc.xyz@bridgelabz.co.in";
-        String regex = "^[a-z0-9]+([.+_-][a-z]+)*@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$";git
+        String regex = "^[a-z0-9]+([.+_-][a-z]+)*@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (matcher.matches())
