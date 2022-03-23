05578808a4f1e48ad1589873fd24af7661192cdc
LDeepakReddy <l.deepakreddy.com>
2022-03-24 01:13:24 +0530

    Added: Welcome message to the program

---
+++ src/com/blz/java/regularexpression/Email.java
@@ -0,0 +1,9 @@
+package com.blz.java.regularexpression;
+
+public class Email {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Email Validation program");
+
+    }
+}
---
+++ src/com/blz/java/regularexpression/PinCode.java
@@ -0,0 +1,12 @@
+package com.blz.java.regularexpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinCode {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Pincode pattern validation program");
+
+    }
+}
+
