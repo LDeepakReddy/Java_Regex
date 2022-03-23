3c1fec48880972d90d2802cf71368086edcdad1a
LDeepakReddy <l.deepakreddy.com>
2022-03-24 02:59:44 +0530

    Added functionality to validate Email with pattern

--- src/com/blz/java/regularexpression/Email.java
+++ src/com/blz/java/regularexpression/Email.java
@@ -1,9 +1,20 @@
 package com.blz.java.regularexpression;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Email {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Email Validation program");
+        String email="abc@bridgelabz.co";
+        String regex="^[a-z]+(@[a-z]+.[a-z]{2,3})*$";
 
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches())
+            System.out.println("email is valid");
+        else
+            System.out.println("email is invalid");
     }
 }
