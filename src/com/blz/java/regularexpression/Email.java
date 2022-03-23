db5cd26a49202952429ee495d528c49c3567f09f
LDeepakReddy <l.deepakreddy.com>
2022-03-24 03:26:58 +0530

    Added Functionality to validate supporting optional parts in the end

--- src/com/blz/java/regularexpression/Email.java
+++ src/com/blz/java/regularexpression/Email.java
@@ -7,9 +7,9 @@ public class Email {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Email Validation program");
-        String email="abc.xyz@bridgelabz.co.in";
-        String regex="^[a-z0-9]+[.+_-]([a-z]+)*@[a-z]+.[a-z]{2,3}[._+-][a-z]{2}$";
-
+        String email = "abc.xyz@bridgelabz.co.in";
+        String regex = "^[a-z0-9]+([.+_-][a-z]+)*@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$";
+        //creating objects of pattern and matcher.
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (matcher.matches())
