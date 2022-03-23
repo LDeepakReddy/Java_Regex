38a1f1381e0da9bf4a0a0652f13c5d75ede5c793
LDeepakReddy <l.deepakreddy.com>
2022-03-24 02:32:42 +0530

    Added  code to validate the pincode with alphabets

--- src/com/blz/java/regularexpression/PinCode.java
+++ src/com/blz/java/regularexpression/PinCode.java
@@ -7,11 +7,10 @@ public class PinCode {
     public static void main(String[] args) {
         System.out.println("Welcome to the Pincode pattern validation program");
         String pinCode = "502303";
-        String regex = "^[0-9]{6}$";
-
+        String regex = "^[1-9]{1}[0-9]{5}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(pinCode);
-        if (matcher.matches())
+        if(matcher.matches())
             System.out.println("PinCode is Valid");
         else
             System.out.println("PinCode is Invalid");
