8c60a351bd88098ee9b7b379c3c46ae5ea0bb5b5
LDeepakReddy <l.deepakreddy.com>
2022-03-24 02:22:15 +0530

    Added Pincode pattern checking functionality

--- src/com/blz/java/regularexpression/PinCode.java
+++ src/com/blz/java/regularexpression/PinCode.java
@@ -6,7 +6,15 @@ import java.util.regex.Pattern;
 public class PinCode {
     public static void main(String[] args) {
         System.out.println("Welcome to the Pincode pattern validation program");
+        String pinCode = "502303";
+        String regex = "^[0-9]{6}$";
 
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pinCode);
+        if (matcher.matches())
+            System.out.println("PinCode is Valid");
+        else
+            System.out.println("PinCode is Invalid");
     }
 }
 
